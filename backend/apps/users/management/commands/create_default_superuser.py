d02052797df6b4839f7f797dbcc44c6787b948e0
intensealchemist <162778488+intensealchemist@users.noreply.github.com>
2026-01-30 00:36:06 +0530

    feat: auto-create superuser on deployment from env variables

---
+++ backend/apps/users/management/commands/create_default_superuser.py
@@ -0,0 +1,36 @@
+"""
+Management command to create a default superuser if it doesn't exist.
+This is useful for automated deployments.
+"""
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+from decouple import config
+
+
+class Command(BaseCommand):
+    help = 'Create a default superuser if one does not exist'
+
+    def handle(self, *args, **options):
+        User = get_user_model()
+        
+        email = config('DJANGO_SUPERUSER_EMAIL', default='')
+        password = config('DJANGO_SUPERUSER_PASSWORD', default='')
+        
+        if not email or not password:
+            self.stdout.write(self.style.WARNING(
+                'DJANGO_SUPERUSER_EMAIL and DJANGO_SUPERUSER_PASSWORD not set. Skipping superuser creation.'
+            ))
+            return
+        
+        if User.objects.filter(email=email).exists():
+            self.stdout.write(self.style.SUCCESS(f'Superuser with email {email} already exists.'))
+            return
+        
+        User.objects.create_superuser(
+            email=email,
+            password=password,
+            first_name='Admin',
+            last_name='User',
+            role='admin'
+        )
+        self.stdout.write(self.style.SUCCESS(f'Superuser created: {email}'))
