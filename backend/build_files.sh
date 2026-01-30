fb486de2cf3645bbee67def5916923e18c3ac2c1
intensealchemist <162778488+intensealchemist@users.noreply.github.com>
2026-01-30 12:46:08 +0530

    feat: Add `build_files.sh` script to automate backend setup and deployment tasks.

--- backend/build_files.sh
+++ backend/build_files.sh
@@ -1,8 +1,7 @@
 # build_files.sh
 echo "Building project..."
 cd backend
-python3.9 -m pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput --clear
-python3.9 manage.py migrate --noinput
-python3.9 manage.py create_default_superuser
+python3 manage.py collectstatic --noinput --clear
+python3 manage.py migrate --noinput
+python3 manage.py create_default_superuser
 echo "Build completed!"
